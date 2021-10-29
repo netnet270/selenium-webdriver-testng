acc6eabcef919b8f525756d73495abcf961f46f6
netnt270 <netnguyen270@gmail.com>
2021-10-30 02:29:32 +0700

    Web-Element-Command-Part-III

--- selenium/webdriver/Topic_03_Xpath_Css_Part_II.java
+++ selenium/webdriver/Topic_03_Xpath_Css_Part_II.java
@@ -1,5 +1,6 @@
 package webdriver;
 
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -15,17 +16,26 @@ public class Topic_03_Xpath_Css_Part_II {
 	WebDriver driver;
 	String projectPath = System.getProperty("user.dir");
 	
+	String firstname, lastname, email, password, fullname;
+
 	@BeforeClass
 	public void beforeClass() {
 		System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
-		driver.get("http://live.techpanda.org/");
+		
+		firstname = "John";
+		lastname = "Smith";
+		email = "john" + getRandomNumber() + "@gmail.com";
+		password = "123456";
+		fullname = firstname + " " + lastname;
 	}
 	
 	@Test
 	public void TC_01_Login_Empty_Data() {
+		driver.get("http://live.techpanda.org/");
+
 		driver.findElement(By.xpath("//div[@class='footer']//a[text()='My Account']")).click();
 			
 		driver.findElement(By.id("email")).sendKeys("");
@@ -45,7 +55,7 @@ public class Topic_03_Xpath_Css_Part_II {
 		driver.findElement(By.id("pass")).sendKeys("123456");
 		driver.findElement(By.cssSelector(".registered-users button[name='send']")).click();
 		
-		String entryEmailText = driver.findElement(By.id("advice-required-entry-email")).getText();
+		String entryEmailText = driver.findElement(By.id("advice-validate-email-email")).getText();
 		Assert.assertEquals(entryEmailText, "Please enter a valid email address. For example johndoe@domain.com.");
 	}
 
@@ -58,7 +68,7 @@ public class Topic_03_Xpath_Css_Part_II {
 		driver.findElement(By.id("pass")).sendKeys("123");
 		driver.findElement(By.cssSelector(".registered-users button[name='send']")).click();
 		
-		String entryPassText = driver.findElement(By.id("advice-required-entry-pass")).getText();
+		String entryPassText = driver.findElement(By.id("advice-validate-password-pass")).getText();
 
 		Assert.assertEquals(entryPassText, "Please enter 6 or more characters without leading or trailing spaces.");
 	}
@@ -82,27 +92,32 @@ public class Topic_03_Xpath_Css_Part_II {
 		driver.findElement(By.xpath("//div[@class='footer']//a[text()='My Account']")).click();
 		driver.findElement(By.xpath("//span[contains(text(),'Create an Account')]")).click();
 		
-		driver.findElement(By.id("firstname")).sendKeys("John");
-		driver.findElement(By.id("middlename")).sendKeys("Smith");
-		driver.findElement(By.id("lastname")).sendKeys("One");
-		driver.findElement(By.id("email_address")).sendKeys("johh@gmail.com");
-		driver.findElement(By.id("password")).sendKeys("123456");
-		driver.findElement(By.id("confirmation")).sendKeys("123456");
+		driver.findElement(By.id("firstname")).sendKeys(firstname);
+		driver.findElement(By.id("lastname")).sendKeys(lastname);
+		driver.findElement(By.id("email_address")).sendKeys(email);
+		driver.findElement(By.id("password")).sendKeys(password);
+		driver.findElement(By.id("confirmation")).sendKeys(password);
 		driver.findElement(By.xpath("//button[@title='Register']")).click();
-		
+
 		Assert.assertEquals(driver.findElement(By.xpath("//li[@class='success-msg']//span")).getText(), "Thank you for registering with Main Website Store.");
 		
-		boolean firstName = driver.findElement(By.xpath("h3[text()='Contact Information']/ancestor::div[@class='box']/div[@class='box-content']/p[contains(text(), 'John Smith One')]")).isDisplayed();
-		boolean Email = driver.findElement(By.xpath("//h3[text()='Contact Information']/ancestor::div[@class='box']/div[@class='box-content']/p[contains(., 'johh@gmail.com')]")).isDisplayed();
+		// Cach 1
+		Assert.assertTrue(driver.findElement(By.xpath("//h3[text()='Contact Information']/ancestor::div[@class='box']/"
+				+ "div[@class='box-content']/p[contains(text(),'"+ fullname+"')]")).isDisplayed());
+		Assert.assertTrue(driver.findElement(By.xpath("//h3[text()='Contact Information']/ancestor::div[@class='box']/"
+				+ "div[@class='box-content']/p[contains(., '"+ email +"')]")).isDisplayed());
 		
-		Assert.assertTrue(firstName);
-		Assert.assertTrue(Email);
+		// Cach 2
+		String contactInfomation = driver.findElement(By.xpath("//h3[text()='Contact Information']/ancestor::div[@class='box']/div[@class='box-content']/p")).getText();
+		
+		Assert.assertTrue(contactInfomation.contains(fullname));
+		Assert.assertTrue(contactInfomation.contains(email));
 		
 		driver.findElement(By.xpath("//a[@class='skip-link skip-account']")).click();
 		driver.findElement(By.xpath("//a[text()='Log Out']")).click();
 		
 		String loginPageTitle = driver.getTitle();
-		Assert.assertEquals(loginPageTitle, "Customer Login");
+		Assert.assertEquals(loginPageTitle, "Magento Commerce");
 	}
 	
 	@Test
@@ -111,19 +126,20 @@ public class Topic_03_Xpath_Css_Part_II {
 
 		driver.findElement(By.xpath("//div[@class='footer']//a[text()='My Account']")).click();
 		
-		driver.findElement(By.id("email")).sendKeys("johnnn@gmail.com");
-		driver.findElement(By.id("pass")).sendKeys("123456");
+		driver.findElement(By.id("email")).sendKeys(email);
+		driver.findElement(By.id("pass")).sendKeys(password);
 		driver.findElement(By.cssSelector(".registered-users button[name='send']")).click();
+			
+		String contactInfomation = driver.findElement(By.xpath("//h3[text()='Contact Information']/ancestor::div[@class='box']/div[@class='box-content']/p")).getText();
 		
-		Assert.assertEquals(driver.findElement(By.xpath("//li[@class='success-msg']//span")).getText(), "Thank you for registering with Main Website Store.");
-		
-		boolean firstName = driver.findElement(By.xpath("h3[text()='Contact Information']/ancestor::div[@class='box']/div[@class='box-content']/p[contains(text(), 'John Smith One')]")).isDisplayed();
-		boolean Email = driver.findElement(By.xpath("//h3[text()='Contact Information']/ancestor::div[@class='box']/div[@class='box-content']/p[contains(., 'johh@gmail.com')]")).isDisplayed();
-		
-		Assert.assertTrue(firstName);
-		Assert.assertTrue(Email);
+		Assert.assertTrue(contactInfomation.contains(fullname));
+		Assert.assertTrue(contactInfomation.contains(email));
 	}
 
+	public int getRandomNumber() {
+		Random rand = new Random();
+		return rand.nextInt(9999);
+	}
 
 	@AfterClass
 	public void afterClass() {
